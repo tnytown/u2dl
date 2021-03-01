6826f8b2e890ea5df934357adf4516faf8e58c2b
Andrew Pan <known@unown.me>
2021-03-01 01:10:21 -0600

    treewide: correct emacs indentation
    don't judge me i swear i had (setq indent-tabs-mode nil) at some point

--- minify.fish
+++ minify.fish
@@ -3,11 +3,11 @@
 set ident_num 0
 set ident_chars (string split "" "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 function gen_ident
-	set -l ident ""
-	set -l n $ident_num
+    set -l ident ""
+    set -l n $ident_num
     
     while true
-		set -a ident $ident_chars[(math $n % 52 + 1)]
+        set -a ident $ident_chars[(math $n % 52 + 1)]
         set n (math -s0 $n / 52)
         
         test $n -eq 0 && break
@@ -18,7 +18,7 @@ function gen_ident
 end
 
 if test (count $argv) -ne 1 || not test -f $argv[1]
- 	echo "usage:" (status -f) "<filename>" >&2; exit 1
+    echo "usage:" (status -f) "<filename>" >&2; exit 1
 end
 
 set -l rules ""
@@ -33,18 +33,18 @@ set idents (echo $content | sed -nE 's/.*((set '$grp_progn_flags')|for |alias )(
 set -a idents 'set' # minify `set` calls
 set -l new_set_ident ""
 for ident in $idents
-	set -l new_ident (gen_ident)
+    set -l new_ident (gen_ident)
     while grep -oE "set $grp_progn_flags$new_ident$not_ident_char" (echo $content | psub) >/dev/null
-		set new_ident (gen_ident)
+        set new_ident (gen_ident)
     end
 
     if test "$ident" = "set"
-		set new_set_ident $new_ident
+        set new_set_ident $new_ident
     end
-	echo $ident -\> $new_ident >&2
+    echo $ident -\> $new_ident >&2
     # general variable rules
-	set -a rules "s/\\\$$ident(($cls_not_ident_char)|\$)/\$$new_ident\2/g" # $ident -> $new_ident
-	set -a rules "s/.*(set $grp_progn_flags)$ident($cls_not_ident_char)/\1$new_ident\3/g"
+    set -a rules "s/\\\$$ident(($cls_not_ident_char)|\$)/\$$new_ident\2/g" # $ident -> $new_ident
+    set -a rules "s/.*(set $grp_progn_flags)$ident($cls_not_ident_char)/\1$new_ident\3/g"
 
     # `for` rules
     set -a rules "s/for $ident in/for $new_ident in/g"
@@ -67,14 +67,14 @@ set -a rules 's/ *([>]) */\1/g' # compress symbols
 
 # apply one by one -- there's some sed parsing issue when we join the statements
 for rule in $rules[2..-1]
-	set content (echo $content | sed -E $rule | string collect)
+    set content (echo $content | sed -E $rule | string collect)
 end
 
 # e e 'alias' ... -> e
 set new_alias_ident (echo $content | sed -En 's/('$cls_ident_char'+) '$cls_ident_char'+ \'alias\'.*/\1/p')
 echo $new_alias_ident >&2
 echo $content | sed "1a\\
-$new_alias_ident $new_set_ident 'set -g';" | tr '\n' ';' |
+	$new_alias_ident $new_set_ident 'set -g';" | tr '\n' ';' |
 #echo 's/('$cls_ident_char'+) \1 \'alias\';alias/alias \1 \'alias\';\1/' >&2
 
 # compressing the `set` usage of `alias` and fixing the `alias` definition
--- split.fish
+++ split.fish
@@ -2,7 +2,7 @@
 
 set in $argv[1]
 for x in (seq 0 (math -s0 \((stat -f '%z' $in)\) / 280))
-	echo -e (set_color -u cyan)"\nnumber $x"(set_color normal)
-	cat $argv[1] | dd if=/dev/stdin of=/dev/stdout bs=1 skip=(math $x '*' 280) count=280 2>/dev/null | tee /dev/
+    echo -e (set_color -u cyan)"\nnumber $x"(set_color normal)
+    cat $argv[1] | dd if=/dev/stdin of=/dev/stdout bs=1 skip=(math $x '*' 280) count=280 2>/dev/null | tee /dev/
     
 end
